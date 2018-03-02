8aa756008a18a6e2753c333bad961e50d99f2b8a
James Marcus <james.marcus9@gmail.com>
2018-03-01 23:56:59 -0500

    corrected tests all pass

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,22 @@
-var repeatString = function() {
-
+var repeatString = function(string, num) {
+  
+    if (num < 0) return 'ERROR';
+    
+    let repeatString = '';
+    
+    let i = 0;
+    
+    while (num > 0 ) {
+        
+        repeatString += string;
+        
+        num--;
+        
+    }
+   
+    
+    return(repeatString);
+    
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-var reverseString = function() {
+var reverseString = function(str) {
 
+    let str =''
+    
+    let strReverse = str.split('').reverse().join('');
+    
+    return strReverse;
+    
 }
 
 module.exports = reverseString
