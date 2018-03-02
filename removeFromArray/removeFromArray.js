f4a4e9142506cf7282e5bee4ffe5f3dd094ee540
James Marcus <james.marcus9@gmail.com>
2018-03-02 11:56:38 -0500

    all tests pas

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-var removeFromArray = function() {
+var removeFromArray = function(...args) {
 
+    const array = args[0];
+    return array.filter(val => !args.includes(val))
 }
 
 module.exports = removeFromArray
