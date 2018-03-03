5932d2a3ea9f66ae4df4eeafa4686ab4b8f114e8
James Marcus <james.marcus9@gmail.com>
2018-03-02 22:38:38 -0500

    all tests passed

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,39 @@
-var sumAll = function() {
+var sumAll = function(firstNumber, secondNumber) {
+    
+if (firstNumber >=0 && secondNumber >= 0) {
+    
+    if (typeof firstNumber !== "number" || typeof secondNumber !== "number") return "ERROR";
+    
+    let sum = 0;
+    
+    if (firstNumber <= secondNumber) {
+            
+            for (i = firstNumber; i <=secondNumber; i++) {
+        
+            
+            sum += i;
+            }
+   
+    } else if (secondNumber < firstNumber) {
+            
+            for (i = secondNumber; i <=firstNumber; i++) {
+        
+            sum += i;
+            }
+    
+    } else {
+            
+            return 'ERROR';
+        
+    }
+     
+        return sum;
 
-}
+}   else {
+    
+    return 'ERROR';
+} 
+
+} 
 
 module.exports = sumAll
