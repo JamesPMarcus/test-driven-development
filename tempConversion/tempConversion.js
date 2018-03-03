0960d5022c0a691d020c62fc6553046edd6b34c5
James Marcus <james.marcus9@gmail.com>
2018-03-02 23:33:39 -0500

    all tests passed

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-var ftoc = function() {
+var ftoc = function(tempF) {
   
+    convertC = (tempF - 32) / 1.8;
+    
+    finalTemp = Math.round( convertC * 10) / 10
+    
+    return finalTemp; 
 }
 
-var ctof = function() {
+var ctof = function(tempC) {
   
+    convertF = (tempC * 1.8) + 32;
+    
+    finalTemp = Math.round( convertF * 10) / 10
+    
+    return finalTemp;
 }
 
 module.exports = {
